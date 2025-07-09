4a4670f7e01142210001162f9a3dbd3b385a2fd4
aglaia <aglaia.hub@gmail.com>
2025-07-09 19:56:47 +0300

    init commit

---
+++ dashboard-service-api/src/main/java/rent/vehicle/dashboardserviceapi/DashboardServiceApiApplication.java
@@ -0,0 +1,13 @@
+package rent.vehicle.dashboardserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardServiceApiApplication.class, args);
+	}
+
+}
---
+++ dashboard-service-api/src/test/java/rent/vehicle/dashboardserviceapi/DashboardServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rent.vehicle.dashboardserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
